85103352a360cbd7065f932a74dd85bc6c1ac545
longstone <github@longstone.ch>
2014-12-18 00:26:26 +0100

    dateparser working

---
+++ app/dist/parser-14util/dateparser.js
@@ -0,0 +1,17 @@
+/**
+ * Created by longstone on 20/11/14.
+ */
+"use strict";
+var assert = require("assert");
+
+var moment = require("moment");
+var dateparser = require("./../../module/parser-14util/dateparser");
+
+describe("test date", function () {
+    it('should parse date as expected', function () {
+        var args = {date: "03.01.2014", time: "17:09"};
+        var actual = dateparser(args);
+        var expected = moment('2014-01-03 17:09');
+        assert.equal(true, expected.isSame(actual));
+    })
+});
