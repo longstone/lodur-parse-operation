18593e0370dbb861aaa50540a2a8af210f7fa210
longstone <github@longstone.ch>
2014-12-21 23:18:30 +0100

    dateparser working

---
+++ app/module/parser-14util/parseGroups.js
@@ -0,0 +1,12 @@
+"use strict";
+var moment = require('moment');
+/**
+ * Created by longstone on 16/12/14.
+ */
+
+module.exports = function parseTimeFromLineF(line) {
+    var startIndex = line.indexOf('/') + 2;
+    var endIndex = line.substring(startIndex, line.length).indexOf('/') - 1 + startIndex;
+    var result = line.substring(startIndex, endIndex)
+    return result.split("+");
+};
