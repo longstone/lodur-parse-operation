3d238d6c8acd7c9a7439842479395f8d09f060c1
longstone <git@longstone.ch>
2015-03-25 08:53:05 +0100

    cron for openshift

---
+++ .openshift/cron/minutely/date.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+if [ ! -f $OPENSHIFT_DATA_DIR/last_run ]; then
+  touch $OPENSHIFT_DATA_DIR/last_run
+fi
+if [[ $(find $OPENSHIFT_DATA_DIR/last_run -mmin +4) ]]; then #run every 5 mins
+  rm -f $OPENSHIFT_DATA_DIR/last_run
+  touch $OPENSHIFT_DATA_DIR/last_run
+  curl http://lodurparser-longstone.rhcloud.com/update
+  # The command(s) that you want to run every 5 minutes
+fi
