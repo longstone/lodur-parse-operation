e6810143a6781ddfab267614f1f426748e3e31a7
longstone <github@longstone.ch>
2015-03-25 12:16:55 +0100

    default listen for Openshift

--- app.js
+++ app.js
@@ -72,7 +72,11 @@ app.use(function (err, req, res, next) {
         error: {}
     });
 });
-var port = process.env.OPENSHIFT_NODEJS_PORT || 8080;
-app.listen(port);
-console.log("app started, port: " + port);
+var server_port = process.env.OPENSHIFT_NODEJS_PORT || 8080;
+var server_ip_address = process.env.OPENSHIFT_NODEJS_IP || '127.0.0.1';
+
+app.listen(server_port, server_ip_address, function () {
+    console.log("Listening on " + server_ip_address + ", server_port " + server_port)
+});
+
 module.exports = app;
