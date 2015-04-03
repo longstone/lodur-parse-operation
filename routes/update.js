eee946cb4afebd9fc6c77fa9f319508c56016aa5
longstone <git@longstone.ch>
2015-04-03 07:28:29 +0200

    bugfixes

--- routes/update.js
+++ routes/update.js
@@ -14,13 +14,13 @@ router.get('/', function (req, res) {
 
     var success = function sucessF(json) {
 
-
+        var lastEntry = json[0];
         var message = new gcm.Message({
-            collapseKey: result.id,
+            collapseKey: lastEntry.number,
             delayWhileIdle: true,
             timeToLive: 259200,
             data: {
-                message: json[0]
+                message: lastEntry
             }
         });
         var sender = new gcm.Sender(process.env.gcmapikey);
@@ -29,18 +29,18 @@ router.get('/', function (req, res) {
         // ... or retrying
         Device.find({}).exec(function (err, result) {
             console.log(result);
-            if (lastID !== result.id) {
-                lastID = result.id;
+            if (lastID !== lastEntry.number) {
+                lastID = lastEntry.number;
                 result.forEach(function (item) {
 
                     // registrationIds.push(item.deviceId);
                     sender.send(message, item.deviceId, function (err, result) {
                         if (err) {
                             console.error(err);
-                            console.log("result: " + result)
+                            console.log("failed sending, result: " + result)
                         }
                         else {
-                            console.log(JSON.stringify(result) + " - Message: " + JSON.stringify(message));
+                            console.log('sent to' + JSON.stringify(result) + " - Message: " + JSON.stringify(message));
                         }
                     });
                 });
