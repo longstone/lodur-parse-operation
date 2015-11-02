2e05c52bd946feaf66a6927b41f376f8bc3be176
git@longstone.ch <git@longstone.ch>
2015-11-02 14:43:13 +0100

    improve error/update handling

--- routes/update.js
+++ routes/update.js
@@ -10,58 +10,64 @@ var teleBot = require('./../module/telegram/telegramMngr');
 var moment = require('moment');
 var LodurEntry = require('./../schemas/lodurEntry');
 /* GET home page. */
-var lastID = 179;
 var _lastEntryCache = null;
 router.get('/', function (req, res) {
 
-    if (!_lastEntryCache) {
-        LodurEntry.find({}).sort({number: -1}).limit(1).exec(function (err, docs) {
-            if (docs.length === 0) {
-                return;
-            }
-            _lastEntryCache = docs[0]._doc;
-            lastID = _lastEntryCache.number;
-        });
-    }
+    LodurEntry.find({}).sort({number: -1}).limit(1).exec(function (err, docs) {
+        //  console.log('err,docs', err, docs);
+        if (docs.length === 0) {
+            return;
+        }
+        _lastEntryCache = docs[0]._doc;
+        pageloader().then(success, fail);
+    });
     var success = function sucessF(json) {
 
         function getSendArray(json) {
             var sendArr = [];
-            json.forEach(function (item) {
-                if (item.number > lastID) {
+            json.every(function (item) {
+                if (item.number > _lastEntryCache.number) {
                     sendArr.push(item);
+                    // persist new Entry
                     LodurEntry.create({
                         number: item.number,
                         group: item.group,
                         timestamp: item.timestamp,
                         description: item.description
                     }, function (err) {
-                        console.log(err);
+                        console.log('persist new Entry Error', err);
                     });
-                    _lastEntryCache = item;
-                    lastID = _lastEntryCache.number;
+                } else {
+                    return false;
                 }
             });
-
+            if (sendArr[0]) {
+                _lastEntryCache = sendArr[0];
+            }
             return sendArr;
         }
 
         var lastEntries = getSendArray(json);
-        lastEntries.reverse();
-        lastEntries.forEach(function (item) {
-            teleBot("Wer:  " + item.group.toString() + "\n"
-                + "Was:  " + item.description + "\n"
-                + "Wann: " + moment(item.timestamp).locale('de').format('HH:mm DD.MM.YY') + "\n"
-                + "Nummer: " + item.number
-            );
-        });
+        if (lastEntries.length > 0) {
+            lastEntries.reverse();
+            lastEntries.forEach(function (item) {
+                teleBot("Wer:  " + item.group.toString() + "\n"
+                    + "Was:  " + item.description + "\n"
+                    + "Wann: " + moment(item.timestamp).locale('de').format('HH:mm DD.MM.YY') + "\n"
+                    + "Nummer: " + item.number
+                );
+            });
+        }
+        var result = {newEntries: lastEntries};
+        res.json(result);
 
 
     };
     var fail = function failF(err) {
+        console.log('err', err);
         res.json({error: err});
     };
-    pageloader().then(success, fail);
+
 
 });
 
