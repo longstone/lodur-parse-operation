089abaf6df6594878d816dc922c848f70d1367ff
git@longstone.ch <git@longstone.ch>
2016-11-01 13:35:59 +0100

    reduce self update frequency

--- .openshift/cron/minutely/date.sh
+++ .openshift/cron/minutely/date.sh
@@ -2,9 +2,8 @@
 if [ ! -f $OPENSHIFT_DATA_DIR/last_run ]; then
   touch $OPENSHIFT_DATA_DIR/last_run
 fi
-if [[ $(find $OPENSHIFT_DATA_DIR/last_run -mmin +4) ]]; then #run every 5 mins
+if [[ $(find $OPENSHIFT_DATA_DIR/last_run -mmin +14) ]]; then #run every 15 mins
   rm -f $OPENSHIFT_DATA_DIR/last_run
   touch $OPENSHIFT_DATA_DIR/last_run
   curl http://lodurparser-longstone.rhcloud.com/update?silent=true
-  # The command(s) that you want to run every 5 minutes
 fi
