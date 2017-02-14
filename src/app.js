ba7e76bb56c48575e58b5ab30eb44517fff9761e
longstone <github@longstone.ch>
2017-02-14 16:58:17 +0100

    refactor index page

--- src/app.js
+++ src/app.js
@@ -31,8 +31,8 @@ const cookieParser = require('cookie-parser');
 const bodyParser = require('body-parser');
 const mongoose = require('mongoose');
 
-
-const routes = require('./routes/index');
+import RouteIndex from './routes/route-index';
+const pageloader = require('./module/pageloader');
 const updatePage = require('./routes/update');
 const app = express();
 
@@ -54,8 +54,10 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended: false}));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
-
-app.use('/', routes);
+const dependencies = {
+    express, pageloader
+};
+app.use('/', new RouteIndex(depenencies).getRoute());
 app.use('/update', updatePage);
 // catch 404 and forward to error handler
 app.use(function (req, res, next) {
---
+++ src/routes/route-index.js
@@ -0,0 +1,26 @@
+class RouteIndex {
+
+    constructor(dependencies) {
+        this.import = {
+            express: dependencies.express,
+            pageloader: dependencies.pageloader
+        };
+        this.router = this.import.express.Router();
+        this.router.get('/', function (req, res) {
+
+            this.import.pageloader().then(json => {
+                    res.setHeader('charset', 'utf8');
+                    res.setHeader('Content-Length', new Buffer(json).length);
+                    res.json(json);
+                },
+                err => res.json({error: err}));
+
+        });
+    }
+
+    getRoute() {
+        return this.router;
+    }
+}
+
+module.exports = TelegramBotService;
