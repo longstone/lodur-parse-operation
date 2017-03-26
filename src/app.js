b3ee00ab5c51ea3202e41e9d10effa1fd4b4ace6
git@longstone.ch <git@longstone.ch>
2017-03-26 12:02:17 +0200

    fix anoying openshift stuff

--- src/app.js
+++ src/app.js
@@ -139,7 +139,8 @@ app.use(function (err, req, res, next) {
         error: {}
     });
 });
-const server_port = process.env.OPENSHIFT_NODEJS_PORT || process.env.PORT || 8080;
+const server_port = process.env.NODE_PORT || process.env.PORT || 8080;
+// const server_port = process.env.OPENSHIFT_NODEJS_PORT || process.env.PORT || 8080;
 // const server_ip_address = process.env.OPENSHIFT_NODEJS_IP || '127.0.0.1';
 
 app.listen(server_port, function () {
