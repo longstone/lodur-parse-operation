4dc4566095b775cb061b7dbc81cda1f97480bfa7
longstone <git@longstone.ch>
2017-10-14 09:33:30 +0200

    add indexes

---
+++ src/schemas/schemaU.js
@@ -0,0 +1,21 @@
+import _ from 'lodash';
+/**
+ * Schema Utility
+ *
+ */
+class SchemaU {
+    constructor(schema,schemaName) {
+        this.schema = schema    ;
+        schema.on('index', (error) => {
+            // "_id index cannot be sparse"
+            console.log(schemaName+' index: '+ _.get(error,'message','seems ok'));
+        });
+    }
+
+    indexes(indexes) {
+        this.schema.index(indexes);
+    }
+
+
+}
+module.exports = SchemaU;
