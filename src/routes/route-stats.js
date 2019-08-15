60c27df0e6e698d08dc7af9af5f28fd50fa3a9d0
longstone <2110765+longstone@users.noreply.github.com>
2019-08-15 10:45:27 +0200

    Update route-stats.js

--- src/routes/route-stats.js
+++ src/routes/route-stats.js
@@ -4,31 +4,26 @@ class RouteIndex {
         this.import = {
             express: dependencies.express,
             persistenceService: dependencies.persistenceService,
-            pageloader: dependencies.pageloader,
             logger: dependencies.logger
         };
-
     }
 
     getRoute() {
         return (req, res) => {
-            let thisYear = this.import.persistenceService.getEntriesForActualYear();
+                let thisYear = this.import.persistenceService.getEntriesForActualYear();
 
-            // maybe there is something for https://www.dasheroo.com/
+                // maybe there is something for https://www.dasheroo.com/
 
-            res.json({
-                my_statistic: {
-                    type: 'integer',
-                    value: thisYear.size(),
-                    label: 'Einsätze im Aktuellen Jahr'
-                }
-            });
-        },
-            err => res.json({error: err});
-    );
+                res.json({
+                    my_statistic: {
+                        type: 'integer',
+                        value: thisYear.size(),
+                        label: 'Einsätze im Aktuellen Jahr'
+                    }
+                });
+            }
+        }
+}
 
-}
-}
-}
 
 module.exports = RouteIndex;
